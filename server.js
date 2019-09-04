d02feb834cd9f6ef060182182c9da348a0f8ac06
mahi6535@gmail.com <mahi6535@gmail.com>
2019-09-05 01:18:04 +0530

    serverjs updated

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-const app = require("express")();
-const stripe = require("stripe")("sk_test_zyF2y2jXcwOmii029FBiaHff00t0f4CfKk");
+const app = require('express')();
+const stripe = require('stripe')('sk_test_zyF2y2jXcwOmii029FBiaHff00t0f4CfKk');
+var uuid = require('uuid');
+bodyParser = require('body-parser');
 
-app.use(require("body-parser").text());
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/charge", async (req, res) => {
-    console.log(req.body)
-    try {
-      let {status} = stripe.charges.create({
-        amount: 2000,
-        currency: "usd",
-        source: "tok_mastercard", // obtained with Stripe.js
-        description: "Charge for jenny.rosen@example.com"
-      }, {
-        idempotency_key: "b7wmQPqkOsWL1WwE"
-      }, function(err, charge) {
+app.post('/charge', async (req, res) => {
+  console.log(req.body.amount);
+  try {
+    let { status } = stripe.charges.create(
+      {
+        amount: req.body.amount,
+        currency: 'usd',
+        source: 'tok_mastercard', // obtained with Stripe.js
+        description: 'Charge for jenny.rosen@example.com'
+      },
+      {
+        idempotency_key: uuid.v4()
+      },
+      function(err, charge) {
         // asynchronously called
-        console.log("ERROR >>> ", err);
-      });
-  
-      res.json({status});
-    } catch (err) {
-      
-      res.status(500).end(err);
-      
-    }
-  });
+        console.log('ERROR >>> ', err, charge);
+      }
+    );
+    res.json({ status });
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
 
+app.get('/balance', async (req, res) => {
+  const result = await stripe.balance.retrieve();
+  console.log(result);
+  res.json(result);
+});
 
-  app.get("/balance", async (req, res) => {
-    const result = await stripe.balance.retrieve(); 
-    res.send({result});
-    
-  });
-
-
-
-  app.listen(9000, () => console.log("Listening on port 9000")); 
+app.listen(9000, () => console.log('Listening on port 9000'));
